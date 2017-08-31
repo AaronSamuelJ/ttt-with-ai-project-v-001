a212a902fec694edeba25ba93ae22c946d58f32c
Aaron Samuel <aasamuelj@gmail.com>
2017-08-31 21:08:37 +0000

    everything is working

---
+++ lib/board.rb
@@ -0,0 +1,39 @@
+class Board
+  attr_accessor :cells
+  def initialize
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts " -------------------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts " -------------------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+  def position(input)
+    self.cells[input_to_index(input)]
+  end
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  def full?
+    self.cells.all?{|cell| cell == "X" || cell == "O"}
+  end
+  def turn_count
+    self.cells.count{|cell| cell == "X" || cell == "O"}
+  end
+  def taken?(input)
+    # !(self.cells[input_to_index(input)] == " " || self.cells[input_to_index(input)].nil?)
+    self.position(input) == "X" || self.position(input) == "O"
+  end
+  def valid_move?(input)
+    user_input = input_to_index(input)
+    user_input.between?(0,8) && !taken?(input)
+  end
+  def update(input, player)
+      self.cells[input_to_index(input)] = player.token
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,47 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+  extend Players
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+  def current_player
+    self.board.turn_count.even? ? @player_1 : @player_2
+  end
+  def over?
+    draw? || won?
+  end
+  def won?
+    WIN_COMBINATIONS.find do |combo|
+      board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[2]] != " "
+    end
+  end
+  def draw?
+    !won? && board.full?
+  end
+  def winner
+    if won?
+      board.cells[won?[0]]
+    end
+  end
+  def turn
+    board.display
+    user_input = self.current_player.move(@board)
+    self.board.valid_move?(user_input) ? self.board.update(user_input, self.current_player) : turn
+  end
+  def play
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+end
