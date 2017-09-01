2a7f33869fde34cb3d0726558695cac9b6948e3b
Aaron Samuel <aasamuelj@gmail.com>
2017-09-01 13:38:35 -0700

    Create player.rb-minmax.rb

---
+++ player.rb-minmax.rb
@@ -0,0 +1,17 @@
+module Tictac
+  class Player
+    attr_reader :piece, :ui
+
+    def initialize(piece, ui)
+      @piece, @ui = piece, ui
+    end
+
+    def move(board)
+      raise 'Not Implemented'
+    end
+
+    def to_s
+      piece
+    end
+  end
+end
