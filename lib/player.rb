73707becc0fd2b16e9e90512dd6e1d79bbe5ccdb
Aaron Samuel <aasamuelj@gmail.com>
2017-09-01 11:06:55 +0000

    adding minmax code

--- lib/player.rb
+++ lib/player.rb
@@ -3,4 +3,19 @@ class Player
   def initialize(token)
     @token = token
   end
-end 
+  ##########
+  # attr_reader :piece, :ui
+  #
+  #     def initialize(piece, ui)
+  #       @piece, @ui = piece, ui
+  #     end
+  #
+  #     def move(board)
+  #       raise 'Not Implemented'
+  #     end
+  #
+  #     def to_s
+  #       piece
+  #     end
+  ##########
+end
